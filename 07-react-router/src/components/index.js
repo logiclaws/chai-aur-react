64733a83a9d6ab233271bec29035032402273261
logiclaws <ideal_kapoor@outlook.com>
2024-08-07 21:59:04 +0530

    learned the react router and created the pages

---
+++ 07-react-router/src/components/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home/Home";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Contact from "./Contact/Contact";
+import About from "./About/About";
+
+export {Home, Header, Footer, About, Contact}
