03ab270587d9e6b9b3ed9a5f3d787ffd05c68de1
logiclaws <ideal_kapoor@outlook.com>
2024-08-07 22:30:07 +0530

    Learned the new ways of nesting routing and the concept of loader

--- 07-react-router/src/components/index.js
+++ 07-react-router/src/components/index.js
@@ -3,5 +3,6 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Contact from "./Contact/Contact";
 import About from "./About/About";
-
-export {Home, Header, Footer, About, Contact}
+import User from "./User/User";
+import Github from "./Github/Github"
+export {Home, Header, Footer, About, Contact, User, Github}
--- 07-react-router/src/main.jsx
+++ 07-react-router/src/main.jsx
@@ -1,24 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout'
 // import Home from './components/Home/Home'
 // import About from './components/About/About'
 // import Contact from './components/Contact/Contact'
-import {Home, Contact, About} from './components/index'
+import {Home, Contact, About, User, Github} from './components/index'
+import { githubInfoLoader } from './components/Github/Github'
 
-const router = createBrowserRouter([
-  {
-    path: '/', 
-    element: <Layout />,
-    children: [
-      { path:'', element:<Home /> },
-      { path:'about', element:<About /> },
-      { path:'contact', element:<Contact /> }
-    ]
-  }
-])
+/// Way 1: Creating Route
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/', 
+//     element: <Layout />,
+//     children: [
+//       { path:'', element:<Home /> },
+//       { path:'about', element:<About /> },
+//       { path:'contact', element:<Contact /> }
+//     ]
+//   }
+// ])
+
+/// Way 2: Creating Route
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
