f905d697fa385dfcde829cc6a2b2541ab380b24f
logiclaws <ideal_kapoor@outlook.com>
2024-08-12 19:58:31 +0530

    Building todo app with context api and local storage.

---
+++ 10-context-local-todo/src/contexts/TodoContext.js
@@ -0,0 +1,17 @@
+import {createContext, useContext} from 'react'
+
+export const TodoContext = createContext({
+    todos: [
+        {id: 1, todo: "Todo message", isCompleted: false,}
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+export const TodoContextProvider = TodoContext.Provider;
+
+export const useTodo = () =>{
+    return useContext(TodoContext);
+}
